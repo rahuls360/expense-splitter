1f173e33e09dbfca369738a66cea7a2b69978ae9
Rahul Makhija <rahuls360@gmail.com>
2019-04-20 14:54:51 +0530

    refactor code into different components

--- ui/src/components/App.js
+++ ui/src/components/App.js
@@ -1,53 +1,17 @@
 import React, { Component } from "react";
-import { IconButton } from "@material-ui/core";
-import { unstable_Box as Box } from "@material-ui/core/Box";
-import Tooltip from "@material-ui/core/Tooltip";
-import { Add, ArrowUpward, AttachMoney } from "@material-ui/icons";
 
+import Header from "./Header";
 import DataTable from "./DataTable";
 
 export class App extends Component {
   render() {
     return (
       <div className="main-app">
-        <section id="logo">
-          <h1>
-            <img
-              src="https://lh3.googleusercontent.com/obOAez-UFR7JGDiOSIpJchJBEX_yYKO26-S4N4DNhii2iWH1FfhQUnIbN9Jbos-R5NiT"
-              alt="expense-splitter"
-              style={styles.logo}
-            />
-          </h1>
-        </section>
-        <Box display="flex" justifyContent="flex-end">
-          <Tooltip title="Add Expense">
-            <IconButton>
-              <Add />
-            </IconButton>
-          </Tooltip>
-          <Tooltip title="Clear Dues">
-            <IconButton>
-              <AttachMoney />
-            </IconButton>
-          </Tooltip>
-          <Tooltip title="Import Expenses">
-            <IconButton>
-              <ArrowUpward />
-            </IconButton>
-          </Tooltip>
-        </Box>
+        <Header />
         <DataTable />
       </div>
     );
   }
 }
-
-const styles = {
-  logo: {
-    maxHeight: "70px",
-    margin: "0 auto",
-    display: "block"
-  }
-};
 
 export default App;
---
+++ ui/src/components/Header.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { IconButton } from "@material-ui/core";
+import { unstable_Box as Box } from "@material-ui/core/Box";
+import Tooltip from "@material-ui/core/Tooltip";
+import { Add, ArrowUpward, AttachMoney } from "@material-ui/icons";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <>
+        <section id="logo">
+          <h1>
+            <img
+              src="https://lh3.googleusercontent.com/obOAez-UFR7JGDiOSIpJchJBEX_yYKO26-S4N4DNhii2iWH1FfhQUnIbN9Jbos-R5NiT"
+              alt="expense-splitter"
+              style={styles.logo}
+            />
+          </h1>
+        </section>
+        <Box display="flex" justifyContent="flex-end">
+          <Tooltip title="Add Expense">
+            <IconButton>
+              <Add />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Clear Dues">
+            <IconButton>
+              <AttachMoney />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Import Expenses">
+            <IconButton>
+              <ArrowUpward />
+            </IconButton>
+          </Tooltip>
+        </Box>
+      </>
+    );
+  }
+}
+
+const styles = {
+  logo: {
+    maxHeight: "70px",
+    margin: "0 auto",
+    display: "block"
+  }
+};
+
+export default Header;
