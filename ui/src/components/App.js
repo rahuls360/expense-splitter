0307466c7d10784a4de0e2edc3f1a404b2217b07
Rahul Makhija <rahuls360@gmail.com>
2019-04-20 13:00:56 +0530

    add logo

--- ui/src/components/App.js
+++ ui/src/components/App.js
@@ -2,8 +2,26 @@ import React, { Component } from "react";
 
 export class App extends Component {
   render() {
-    return <div>h32</div>;
+    return (
+      <div className="main-app">
+        <h1>
+          <img
+            src="https://lh3.googleusercontent.com/obOAez-UFR7JGDiOSIpJchJBEX_yYKO26-S4N4DNhii2iWH1FfhQUnIbN9Jbos-R5NiT"
+            alt="expense-splitter"
+            style={styles.logo}
+          />
+        </h1>
+      </div>
+    );
   }
 }
+
+const styles = {
+  logo: {
+    maxHeight: "70px",
+    margin: "0 auto",
+    display: "block"
+  }
+};
 
 export default App;
